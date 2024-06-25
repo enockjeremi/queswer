96c65b5f9373fc7c7bd7dfef4588385010d64e61
Enock Jeremi <enockjeremi@gmail.com>
2024-06-24 22:26:09 -0400

    feat(api): Created function to serialize API response, excluding certain fields like QuestionID from answers when returning all Questions

--- controllers/question.controller.go
+++ controllers/question.controller.go
@@ -8,6 +8,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AnswerReponse struct {
+	Content    string `json:"content"`
+	QuestionID uint   `json:"question"`
+}
+
+type QuestionReponse struct {
+	ID          uint            `json:"id"`
+	Title       string          `json:"title"`
+	Description string          `json:"description"`
+	Completed   bool            `json:"completed"`
+	Answers     []AnswerReponse `json:"answers"`
+}
+
+func QuestionSerializer(q []models.Question) []QuestionReponse {
+	var questionResponse []QuestionReponse
+	for _, question := range q {
+		var answerReponse []AnswerReponse
+		for _, answer := range question.Answer {
+			answerReponse = append(answerReponse, AnswerReponse{
+				Content: answer.Content,
+			})
+		}
+		questionResponse = append(questionResponse, QuestionReponse{
+			ID:          question.ID,
+			Title:       question.Title,
+			Description: question.Description,
+			Completed:   question.Completed,
+			Answers:     answerReponse,
+		})
+	}
+	return questionResponse
+}
+
 func GetAllQuestion(c *gin.Context) {
 	var question []models.Question
 	if err := services.FindAllQuestion(&question); err != nil {
@@ -15,7 +48,9 @@ func GetAllQuestion(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, question)
+	response := QuestionSerializer(question)
+
+	c.JSON(http.StatusOK, &response)
 }
 func PostQuestion(c *gin.Context) {
 	var question models.Question
